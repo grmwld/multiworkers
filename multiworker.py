e880dfce2da2fa950fdb31c889fb003ffccb5be2
Alexis Grimaldi <alexis.grimaldi+dev@gmail.com>
2011-05-01 16:29:38 +0200

    Changed all private attributes to semi-private.

--- multiworker.py
+++ multiworker.py
@@ -16,78 +16,76 @@ import numpy
 class Worker(multiprocessing.Process):
     def __init__(self, work_queue, result_queue, verbose):
         super(Worker, self).__init__()
-        self.__work_queue = work_queue
-        self.__result_queue = result_queue
-        self.__kill_received = False
-        self.__verbose = verbose
+        self._work_queue = work_queue
+        self._result_queue = result_queue
+        self._kill_received = False
+        self._verbose = verbose
 
-    def __print_verbose(self, msg):
-        if self.__verbose:
+    def _print_verbose(self, msg):
+        if self._verbose:
             sys.stdout.write(msg)
             sys.stdout.flush()
 
     def run(self):
-        while not self.__kill_received:
+        while not self._kill_received:
             try:
-                job = self.__work_queue.get(True, 0.1)
+                job = self._work_queue.get(True, 0.1)
             except Queue.Empty:
                 break
             self.do(job)
 
-    def do(self, job_params):
-        self.__result_queue.put(
-            job_params
-        )
+    def do(self, job):
+        self._result_queue.put(job)
 
 
 class Controller:
     def __init__(self, input, num_cpu, verbose):
-        self.__input = input
-        self.__num_cpu = num_cpu
-        self.__verbose = verbose
-        self.__work_queue = multiprocessing.Queue()
-        self.__num_jobs = 0
-        for param_set in self.__input:
-            param_set['id'] = self.__num_jobs
-            self.__work_queue.put(param_set)
-            self.__num_jobs += 1
-        self.__result_queue = multiprocessing.Queue()
-        self.__results = []
-        self.__workers = []
-        self.__init_workers()
+        self._input = input
+        self._num_cpu = num_cpu
+        self._verbose = verbose
+        self._work_queue = multiprocessing.Queue()
+        self._num_jobs = 0
+        for param_set in self._input:
+            param_set['id'] = self._num_jobs
+            self._work_queue.put(param_set)
+            self._num_jobs += 1
+        self._result_queue = multiprocessing.Queue()
+        self._results = []
+        self._workers = []
+        self._init_workers()
 
-    def __init_workers(self):
-        for i in range(self.__num_cpu):
+    def _init_workers(self):
+        for i in range(self._num_cpu):
             worker = Worker(
-                    self.__work_queue,
-                    self.__result_queue,
-                    self.__verbose
+                    self._work_queue,
+                    self._result_queue,
+                    self._verbose
             )
-            self.__workers.append(worker)
+            self._workers.append(worker)
 
-    def __finish(self):
-        self.__print_verbose('Finishing ...')
-        self.__results.sort(cmp=lambda x,y: x['id'] - y['id'])
-        for result in self.__results:
+    def _finish(self):
+        self._print_verbose('Finishing ...')
+        self._results.sort(cmp=lambda x,y: x['id'] - y['id'])
+        for result in self._results:
             print result
-        self.__print_verbose(' done !')
+        self._print_verbose(' done !')
 
-    def __print_verbose(self, msg):
-        if self.__verbose:
+    def _print_verbose(self, msg):
+        if self._verbose:
             sys.stdout.write(msg)
             sys.stdout.flush()
 
     def start(self):
         try:
-            for worker in self.__workers:
+            for worker in self._workers:
                 worker.start()
-            while len(self.__results) < self.__num_jobs:
-                result = self.__result_queue.get()
-                self.__results.append(result)
+            while len(self._results) < self._num_jobs:
+                result = self._result_queue.get()
+                self._results.append(result)
         except Exception as err:
             traceback.print_exc()
         finally:
-            self.__finish()
+            self._finish()
 
 
 if __name__ == '__main__':
